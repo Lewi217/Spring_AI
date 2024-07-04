9f322f08f946b3caf5e5d260108f299885178223
lewis wanjohi <wanjohilewi7@gmail.com>
2024-07-04 22:36:24 +0300

    Chatgpt using JAVA

---
+++ src/test/java/Wave/Chatgpt/ChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package Wave.Chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
